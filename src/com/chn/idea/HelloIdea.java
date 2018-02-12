d7d19f1cf3668a2e85eddd97225fbf21e1530067
nickframework <283062915@qq.com>
2018-02-12 16:49:40 +0800

    just soso

--- src/com/chn/idea/HelloIdea.java
+++ src/com/chn/idea/HelloIdea.java
@@ -3,6 +3,7 @@ package com.chn.idea;
 public class HelloIdea {
     public static void main(String[] args) {
         System.out.println("HelloIdea!!!");
-        System.out.println("HelloGitHub!!!");
+        System.out.println("Hello GitHub!!!");
+        System.out.println("Hello master!!");
     }
 }
