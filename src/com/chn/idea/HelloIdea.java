384327313cb9ce7ec028ded1d4479746e35d7474
nickframework <283062915@qq.com>
2018-02-12 15:53:15 +0800

    hellogithub

--- src/com/chn/idea/HelloIdea.java
+++ src/com/chn/idea/HelloIdea.java
@@ -3,5 +3,6 @@ package com.chn.idea;
 public class HelloIdea {
     public static void main(String[] args) {
         System.out.println("HelloIdea!!!");
+        System.out.println("HelloGitHub!!!");
     }
 }
